3702a712f32cac15159663152adcdc58ab42e792
DanG <minigomezdd@gmail.com>
2025-04-03 16:08:54 -0600

    Funcionando Hola Mundo

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+from dotenv import load_dotenv
+
+#Cargar las variables de entorno
+load_dotenv()
+
+#Crear instancia
+app = Flask(__name__)
+
+#Configuración de la base de datos PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicializar SQLAlchemy
+db = SQLAlchemy(app)
+
+#Ruta principal: home
+@app.route('/')
+def index():
+    return 'Hola mundo'
